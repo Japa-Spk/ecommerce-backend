7a795edcbdeb44ae5083d44e72be33933a5b8129
Japa-Spk <japinto08@ucatolica.edu.co>
2024-08-22 20:31:27 -0500

    init ecommerce backend

---
+++ src/main/java/com/ecommerce/backend/service/ProductService.java
@@ -0,0 +1,39 @@
+package com.ecommerce.backend.service;
+
+import com.ecommerce.backend.model.Product;
+import com.ecommerce.backend.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id).orElse(null);
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product product) {
+        if (productRepository.existsById(id)) {
+            product.setId(id);
+            return productRepository.save(product);
+        }
+        return null;
+    }
+
+    public void deleteProduct(Long id) {
+        productRepository.deleteById(id);
+    }
+}
